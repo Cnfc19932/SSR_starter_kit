715d1766a7da8ae3c0382b35178c9ab5e8e2cf1a
Stanislav Gorovoy <cnfc19932@gmail.com>
2019-10-16 23:17:08 +0300

    Единный webpack конфиг

---
+++ config.webpack.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+module.exports = [
+    {
+        name: 'client',
+        entry: path.resolve(__dirname, 'src/client.tsx'),
+        output: {
+            path: path.resolve(__dirname, 'public'),
+            filename: 'client.bundle.js',
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+        },
+        target: 'node',
+        node: {
+            fs: 'empty',
+        },
+        mode: 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                                modules: {
+                                    localIdentName: '[name]__[local]___[hash:base64:5]',
+                                },
+                            },
+                        }
+                    ],
+                },
+                // ts-loader: конвертируем ts (es6) в js (es6)
+                // babel-loader: конвертируем js (es6) в js (es5)
+                {
+                    test: /\.tsx?$/,
+                    loaders: ['babel-loader', 'ts-loader'],
+                    exclude: [/node_modules/, nodeModulesPath],
+                },
+                // babel-loader: конвертируем js (es6) в js (es5)
+                {
+                    test: /\.(jsx?)$/,
+                    loaders: ['babel-loader'],
+                    exclude: [/node_modules/, nodeModulesPath],
+                }
+            ]
+        },
+    },
+    {
+        name: 'server',
+        entry: path.resolve(__dirname, 'src/server.js'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'bundle.js',
+        },
+        resolve: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'],
+        },
+        target: 'node',
+        node: {
+            fs: 'empty',
+        },
+        mode: 'production',
+        module: {
+            rules: [
+                {
+                    test: /\.css$/i,
+                    use: [
+                        {
+                            loader: 'isomorphic-style-loader',
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: {
+                                    localIdentName: '[name]__[local]___[hash:base64:5]',
+                                },
+                                importLoaders: 1,
+                                sourceMap: true
+                            }
+                        },
+                    ],
+                },
+                // ts-loader: конвертируем ts (es6) в js (es6)
+                // babel-loader: конвертируем js (es6) в js (es5)
+                {
+                    test: /\.tsx?$/,
+                    loaders: ['babel-loader', 'ts-loader'],
+                    exclude: [/node_modules/, nodeModulesPath],
+                },
+                // babel-loader: конвертируем js (es6) в js (es5)
+                {
+                    test: /\.(jsx?)$/,
+                    loaders: ['babel-loader'],
+                    exclude: [/node_modules/, nodeModulesPath],
+                }
+            ]
+        },
+    }
+];
